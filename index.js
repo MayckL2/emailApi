9aff617ca3f7b40cb570efa4978ab5be3d65139b
Mayck <103364944+MayckL2@users.noreply.github.com>
2023-12-03 12:54:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,7 @@ app.use((req, res, next) => {
 app.post('/', (req, res)=>{
     const {email, toEmail, subject, text} = req.body
 
-     if(email || toEmail || subject || text){
-        console.log("dados ok")
-     }else{
+     if(email == "" || toEmail == "" || subject == "" || text == ""){
         return res.status(401).send({msg: "Envie email, toEmail, subject e text para enviar email..."})
     }
 
