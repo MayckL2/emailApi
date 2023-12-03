c8ac41a8b3d48d42813ba5bcdeca55cb613868dd
Mayck <103364944+MayckL2@users.noreply.github.com>
2023-12-03 12:34:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const {sendEmailService} = require('./sendEmail') 
 const nodemailer = require('nodemailer')
 
 // habilida dotenv
